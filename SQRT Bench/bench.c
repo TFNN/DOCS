f9a479ccd0bbe319897640e4a16cbbc6c28e9d50
TFCNN <73448936+TFCNN@users.noreply.github.com>
2021-08-18 04:36:21 +0100

    Update bench.c

--- SQRT Bench/bench.c
+++ SQRT Bench/bench.c
@@ -4,11 +4,22 @@
 #include <math.h>
 #include <string.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <xmmintrin.h>
 
 static inline float isqrt(float f)
 {
     return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(f)));
+}
+
+float rndFloat() // forces the -Ofast mode to produce code containing sqrt
+{
+    float ret = 0;
+    int f = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
+    read(f, &ret, sizeof(float));
+    close(f);
+    return ret;
 }
 
 uint64_t microtime()
@@ -34,14 +45,14 @@ int main()
     uint64_t count = 0;
     while(microtime()-st < 3000000)
     {
-        ret = 1 / sqrt(rand());
+        ret += 1 / sqrt(rndFloat());
         count++;
     }
     printf("3 Seconds of Executions: %lu\n", count);
     
     st = microtime();
     for(int i = 0; i < 1000000; i++)
-        ret = 1 / sqrt(rand());
+        ret += 1 / sqrt(rndFloat());
     printf("1,000,000 Exections Microseconds: %lu\n", microtime()-st);
 
     
@@ -53,15 +64,17 @@ int main()
     count = 0;
     while(microtime()-st < 3000000)
     {
-        ret = isqrt(rand());
+        ret += isqrt(rndFloat());
         count++;
     }
     printf("3 Seconds of Executions: %lu\n", count);
     
     st = microtime();
     for(int i = 0; i < 1000000; i++)
-        ret = isqrt(rand());
+        ret += isqrt(rndFloat());
     printf("1,000,000 Exections Microseconds: %lu\n", microtime()-st);
 
+
+    printf("%.0f\n", ret); // forces the -Ofast mode to produce code containing sqrt
     return 0;
 }
