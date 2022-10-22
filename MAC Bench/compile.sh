b7076564e272117a8de1af570d3007aa515e5c68
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-10-22 13:15:15 +0100

    Update compile.sh

--- MAC Bench/compile.sh
+++ MAC Bench/compile.sh
@@ -1,4 +1,4 @@
 gcc bench.c -lm -o fpu
 gcc bench.c -Ofast -lm -o ofast
-gcc bench.c -Ofast -mavx -mfma -lm -o ofast_fma
-gcc bench.c -Ofast -mavx -mfma4 -lm -o ofast_fma4
+gcc bench.c -Ofast -mfma -lm -o ofast_fma
+gcc bench.c -Ofast -mfma4 -lm -o ofast_fma4
